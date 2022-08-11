7a02742cffe38be78ec3630c0b6ef2aff9beac0e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-12 01:32:32 +0530

    add getUserDetails method which will use the user service to get suer details and subscribe to it and assign response to userData variable

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../user.service';
+import { User } from '../user_interface';
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
-  styleUrls: ['./user-info.component.css']
+  styleUrls: ['./user-info.component.css'],
 })
 export class UserInfoComponent implements OnInit {
-
-  constructor() { }
+  userData: User | null = null;
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.getUserDetails();
   }
 
+  getUserDetails() {
+    this.userService
+      .getUserDetail()
+      .subscribe((data) => (this.userData = data));
+  }
 }
